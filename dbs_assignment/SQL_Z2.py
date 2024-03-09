e94f6de20d7fa8d7354363db71d10e981fcbdb19
Alex-hre <hredzhev@gmail.com>
2024-03-09 18:33:54 +0100

    opravy

--- dbs_assignment/SQL_Z2.py
+++ dbs_assignment/SQL_Z2.py
@@ -59,7 +59,7 @@ SELECT posts.id, TO_CHAR(posts.creationdate AT TIME ZONE 'UTC+0', 'YYYY-MM-DD"T"
 	FROM posts
 	JOIN post_tags ON posts.id = post_tags.post_id JOIN tags ON post_tags.tag_id = tags.id
 	WHERE (posts.body ILIKE '%' || unaccent($2) || '%' OR posts.title ILIKE '%' || unaccent($2) || '%')
-	GROUP BY posts.id, posts.body, posts.title, posts.answercount
+	GROUP BY posts.id, posts.creationdate, posts.viewcount, posts.lasteditdate, posts.title, posts.body, posts.answercount, posts.closeddate
 	ORDER BY posts.creationdate DESC
 	LIMIT $1
 """
