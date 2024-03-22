22e65c6e87755ea0b241eb78214d430b2d19f2c9
Alex-hre <hredzhev@gmail.com>
2024-03-22 18:51:24 +0100

    3rd, first commit

--- dbs_assignment/SQL_Z2.py
+++ dbs_assignment/SQL_Z2.py
@@ -58,10 +58,91 @@ GET_POST_ON_KEYWORD_WITH_LIMIT_QUERY = """
 SELECT posts.id, TO_CHAR(posts.creationdate AT TIME ZONE 'UTC+0', 'YYYY-MM-DD"T"HH24:MI:SS.US+00:00') AS creationdate, posts.viewcount, TO_CHAR(posts.lasteditdate AT TIME ZONE 'UTC+0', 'YYYY-MM-DD"T"HH24:MI:SS.US+00:00') AS lasteditdate, TO_CHAR(posts.lastactivitydate AT TIME ZONE 'UTC+0', 'YYYY-MM-DD"T"HH24:MI:SS.US+00:00') AS lastactivitydate, posts.title, posts.body, posts.answercount, TO_CHAR(posts.closeddate AT TIME ZONE 'UTC+0', 'YYYY-MM-DD"T"HH24:MI:SS.US+00:00') AS closeddate, STRING_AGG(tags.tagname, ', ') AS tags_list
 	FROM posts
 	JOIN post_tags ON posts.id = post_tags.post_id JOIN tags ON post_tags.tag_id = tags.id
-	WHERE (posts.body ILIKE '%' || unaccent($2) || '%' OR posts.title ILIKE '%' || unaccent($2) || '%')
+	WHERE (unaccent(posts.body) ILIKE '%' || unaccent($2) || '%' OR posts.title ILIKE '%' || unaccent($2) || '%')+
 	GROUP BY posts.id, posts.creationdate, posts.viewcount, posts.lasteditdate, posts.title, posts.body, posts.answercount, posts.closeddate, posts.lastactivitydate
 	ORDER BY posts.creationdate DESC
 	LIMIT $1
+"""
+
+
+
+
+
+################# Z3
+
+GET_BADGE_POSTS_HISTORY_WITH_LIMIT_QUERY = """
+SELECT id, title, type, created_at, CEIL(ROW_NUMBER() OVER (ORDER BY created_at) / 2.0) AS position
+FROM(
+SELECT *, LAG(type) OVER (ORDER BY created_at) AS previous_type, LEAD(type) OVER (ORDER BY created_at) AS next_type
+FROM(
+	SELECT posts.id AS id,
+       posts.title AS title,
+       'post' AS type,
+       TO_CHAR(posts.creationdate AT TIME ZONE 'UTC+0', 'YYYY-MM-DD"T"HH24:MI:SS.US+00:00') AS created_at
+	FROM posts
+	JOIN users ON posts.owneruserid = users.id
+	WHERE users.id = $1
+
+	UNION
+
+	SELECT badges.id AS id,
+       badges.name AS title,
+       'badge' AS type,
+       TO_CHAR(badges.date AT TIME ZONE 'UTC+0', 'YYYY-MM-DD"T"HH24:MI:SS.US+00:00') AS created_at
+	FROM badges
+	JOIN users ON badges.userid = users.id
+	WHERE users.id = $1
+	ORDER BY created_at
+) as final
+) as finality
+WHERE (previous_type = 'post' AND type = 'badge') OR (next_type = 'badge' AND type = 'post')
+"""
+
+
+
+GET_COMMENTS_BY_TAGS_MORE_THAN_COUNT_QUERY = """
+SELECT post_id, title, displayname, text, TO_CHAR(created_at AT TIME ZONE 'UTC+0', 'YYYY-MM-DD"T"HH24:MI:SS.US+00:00') AS created_at, (created_at - previous_time) AS diff, AVG(created_at - previous_time) OVER (PARTITION BY post_id ORDER BY created_at ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS avg
+FROM(
+	SELECT users.displayname AS displayname, posts.id AS post_id, posts.title AS title, comments.text AS text, comments.creationdate AS created_at, posts.creationdate, LAG(comments.creationdate, 1, posts.creationdate) OVER (PARTITION BY posts.id ORDER BY comments.creationdate) AS previous_time
+	FROM post_tags
+		JOIN posts ON post_tags.post_id = posts.id
+		JOIN tags ON tags.id = post_tags.tag_id
+		JOIN comments ON posts.id = comments.postid
+		LEFT JOIN users ON comments.userid = users.id
+
+	WHERE tags.tagname = $1 AND posts.commentcount > $2
+	ORDER BY comments.creationdate
+)
+"""
+
+
+GET_K_COMMENT_TO_POSTS_BY_TAGS_WITH_LIMIT_QUERY = """
+    SELECT comments.id, users.displayname, base.body, comments.text, comments.score, ROW_NUMBER() OVER () AS position  -- TO_CHAR(comments.creationdate AT TIME ZONE 'UTC+0', 'YYYY-MM-DD"T"HH24:MI:SS.US+00:00') AS creationdate,
+    FROM comments
+    JOIN users ON comments.userid = users.id
+    JOIN(
+        SELECT posts.id as post_id, posts.body, ARRAY_AGG(comments.id ORDER BY comments.creationdate) AS comment_ids
+        FROM tags
+            JOIN post_tags ON tags.id = post_tags.tag_id
+            JOIN posts ON post_tags.post_id = posts.id
+            JOIN comments ON posts.id = comments.postid
+        WHERE tags.tagname = $1
+        GROUP BY posts.id, posts.body
+        HAVING COUNT(*) >= $2
+        ORDER BY posts.creationdate
+        LIMIT $3
+    ) base ON comments.id = base.comment_ids[$2]
+"""
+
+
+
+GET_POSTS_AND_PARENT_POSTS_BY_POSTID_WITH_LIMIT_QUERY = """
+SELECT users.displayname, posts.body, TO_CHAR(posts.creationdate AT TIME ZONE 'UTC+0', 'YYYY-MM-DD"T"HH24:MI:SS.US+00:00') AS created_at
+FROM posts
+JOIN users ON posts.owneruserid = users.id
+WHERE posts.id = $1 OR posts.parentid = $1
+ORDER BY posts.creationdate
+LIMIT $2
 """
 
 
--- dbs_assignment/endpoints/hello.py
+++ dbs_assignment/endpoints/hello.py
@@ -3,7 +3,9 @@ from typing import Optional
 from fastapi import APIRouter, Query
 
 from dbs_assignment.SQL_Z2 import GET_POST_USERS_QUERY, GET_FRIENDS_USERS_QUERY, GET_PERCENTAGE_TAGS_QUERY, \
-    GET_POST_DURATION_WITH_LIMIT_QUERY, GET_POST_ON_KEYWORD_WITH_LIMIT_QUERY
+    GET_POST_DURATION_WITH_LIMIT_QUERY, GET_POST_ON_KEYWORD_WITH_LIMIT_QUERY, GET_BADGE_POSTS_HISTORY_WITH_LIMIT_QUERY, \
+    GET_K_COMMENT_TO_POSTS_BY_TAGS_WITH_LIMIT_QUERY, GET_POSTS_AND_PARENT_POSTS_BY_POSTID_WITH_LIMIT_QUERY, \
+    GET_COMMENTS_BY_TAGS_MORE_THAN_COUNT_QUERY
 from dbs_assignment.database_connect import get_postgres_version, execute_query
 
 
@@ -213,6 +215,104 @@ async def get_posts(limit: int, duration: Optional[int] = None, query: Optional[
             formatted_posts.append(formatted_post)
         response_data = {"items": formatted_posts}
         return response_data
+
+############################### Z3
+
+
+@router.get('/v3/users/{user_id}/badge history')     ### /v3/users/120/badge history
+async def get_posts_and_badges(user_id: int):
+    posts_and_badges = await execute_query(GET_BADGE_POSTS_HISTORY_WITH_LIMIT_QUERY, user_id)
+
+    formatted_posts_and_badges = []
+    for post_and_badge in posts_and_badges:
+
+
+        post_and_badge = {
+            "id": post_and_badge["id"],
+            "title": str(post_and_badge["title"]),
+            "type": str(post_and_badge["type"]),
+            "created_at": str(post_and_badge["created_at"]),
+            "position": post_and_badge["position"],
+        }
+        formatted_posts_and_badges.append(post_and_badge)
+
+
+    response_data = {"result": formatted_posts_and_badges}
+
+    return response_data
+
+
+
+@router.get('/v3/tags/{tag}/comments')     ### /v3/tags/networking/comments?count=40
+async def get_comments_more_than_count(tag: str, count: int):
+    comments_more_than_count = await execute_query(GET_COMMENTS_BY_TAGS_MORE_THAN_COUNT_QUERY, tag, count)
+
+    formatted_comments_more_than_count = []
+    for comment in comments_more_than_count:
+
+        comment = {
+            "post_id": comment["post_id"],
+            "title": str(comment["title"]),
+            "displayname": str(comment["displayname"]),
+            "text": str(comment["text"]),
+            "created_at": str(comment["created_at"]),
+            "diff": str(comment["diff"]),
+            "avg": str(comment["avg"])
+        }
+        formatted_comments_more_than_count.append(comment)
+
+    response_data = {"result": formatted_comments_more_than_count}
+
+    return response_data
+
+
+
+
+@router.get('/v3/tags/{tagname}/comments/{position}')   ### /v3/tags/linux/comments/2?limit=1
+async def get_comments(tagname: str, position: int, limit: int ):
+
+    comments = await execute_query(GET_K_COMMENT_TO_POSTS_BY_TAGS_WITH_LIMIT_QUERY, tagname, position, limit)
+
+    formatted_comments = []
+    for comment in comments:
+
+        formatted_comment = {
+            "id": comment["id"],
+            "displayname": str(comment["displayname"]),
+            "body": str(comment["body"]),
+            "text": str(comment["text"]),
+            "score": comment["score"],
+            "position": comment["position"]
+        }
+        formatted_comments.append(formatted_comment)
+    response_data = {"items": formatted_comments}
+    return response_data
+
+
+
+
+
+
+@router.get('/v3/posts/{post_id}')   ### /v3/posts/2154?limit=2
+async def get_posts_tree(post_id: int, limit: int):
+
+    posts_tree = await execute_query(GET_POSTS_AND_PARENT_POSTS_BY_POSTID_WITH_LIMIT_QUERY, post_id, limit)
+
+    formatted_posts_tree = []
+    for post in posts_tree:
+
+        formatted_comment = {
+            "displayname": str(post["displayname"]),
+            "body": str(post["body"]),
+            "created_at": str(post["created_at"])
+        }
+        formatted_posts_tree.append(formatted_comment)
+    response_data = {"items": formatted_posts_tree}
+    return response_data
+
+
+
+
 
 
 
